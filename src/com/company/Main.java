ab35e87a1c3631eeb37929f66fc8dac56f5437b1
izczum <izabelaczumak@student.kul.lublin.pl>
2021-03-31 19:20:19 +0200

    my second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iza test");
         System.out.println("mój drugi commit");
+        System.out.println("mój trzeci commit");
 	// write your code here
     }
 }
